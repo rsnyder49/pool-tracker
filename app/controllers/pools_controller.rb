75754fd6cdb5990b97cc94e9929b476a03649b78
Robert Snyder <r.snyder49@gmail.com>
2018-11-14 02:54:48 +0000

    modified pools_controller

--- app/controllers/pools_controller.rb
+++ app/controllers/pools_controller.rb
@@ -26,4 +26,33 @@ class PoolController < ApplicationController
     erb :'pools/show_pool'
   end
   
+  post '/tweets' do
+    @user = Helpers.current_user(session)
+    if (params.has_value?(""))
+      redirect to '/pools/new'
+    elsif !params.empty?
+      @pool = Tweet.create(:content => params[:content])
+      @user = User.find_by(params[:id])
+      @pool.user_id = @user.id
+      @pool.save
+      @user.pools << @pool
+    end
+    redirect to "/tweets/#{@pool.id}"
+  end
+  
+  delete '/pools/:id/delete' do
+    @pool = Pool.find_by_id(params[:id])
+    if @pool.cleaner != Helper.current_user(session)
+      redirect to '/pools'
+    end
+    if Helper.logged_in?(session)
+      if @pool.user_id == Helper.current_user.id
+        @pool.destroy
+        redirect to '/pools'
+      end
+    else
+      redirect to '/login'
+    end
+  end
+  
 end 
