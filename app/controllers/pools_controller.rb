310f7f6aa16c6189b7264cb616e2306bedc2e9e8
Robert Snyder <r.snyder49@gmail.com>
2018-11-14 03:08:39 +0000

    modifed pools_controller.erb and views/pools

--- app/controllers/pools_controller.rb
+++ app/controllers/pools_controller.rb
@@ -55,4 +55,31 @@ class PoolController < ApplicationController
     end
   end
   
+  get '/pools/:id/edit' do
+    if Helper.logged_in?(session)
+      @pool = Pool.find_by_id(params[:id])
+    else
+      redirect to '/login'
+    end
+    erb :'pools/edit_pool'
+  end
+  
+  patch '/pools/:id' do
+    @pool = Pool.find_by_id(params[:id])
+    if @pool.cleaner != Helper.current_user(session)
+      redirect to '/pools'
+    end
+    if !(params.has_value?(""))
+      @pool.update(content: params[:content])
+      @pool.save
+     else
+      redirect to "/pools/#{@pool.id}/edit"
+    end
+    redirect to "/pools/#{@pool.id}"
+  end
+  
+  
 end 
+
+
+
