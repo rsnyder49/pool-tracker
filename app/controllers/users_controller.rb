d3e7f5456dc88ffbb8d44f9819b8e67b6e94a50a
Robert Snyder <r.snyder49@gmail.com>
2018-11-15 00:50:11 +0000

    modified db/user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,54 +1,55 @@
-# class UsersController < ApplicationController 
+class UsersController < ApplicationController 
   
-#   get '/signup' do 
-#     if Helper.logged_in?(session)
-#       redirect '/pools'
-#     end 
-#     erb :create_user
-#   end 
+  get '/signup' do 
+    if Helper.logged_in?(session)
+      redirect '/pools'
+    end 
+    erb :create_user
+  end 
   
-#   post '/signup' do 
-#     if !params.has_value?("") && params[:email].include?("@")
-#       @user = User.create(name: params[:username], email: params[:email], password: params[:password])
-#       @user[:user_id] = @user.id
-#       redirect '/pools'
-#     else 
-#       redirect 'signup'
-#   end 
+  post '/signup' do 
+    if !params.has_value?("") && params[:email].include?("@")
+      @user = User.create(name: params[:username], email: params[:email], password: params[:password])
+      @user[:user_id] = @user.id
+      redirect '/pools'
+    else 
+      redirect 'signup'
+    end
+  end 
   
-#   get '/login' do 
-#     if Helper.logged_in?(session)
-#       redirect '/pools'
-#     else 
-#       erb :'users/login'
-#     end 
-#   end
+  get '/login' do 
+    if Helper.logged_in?(session)
+      redirect '/pools'
+    else 
+      erb :'users/login'
+    end 
+  end
   
-#   # post '/login' do 
-#   #   @user = User.find_by(username => params[:username]
-#   #   if @user && @user.authenticate(params[:password])
-#   #     session[:user_id] = @user.id 
-#   #     redirect '/pools'
-#   #   else 
-#   #     redirect '/login' 
-#   #   end 
-#   # end
+  post '/login' do 
+    @user = User.find_by(username => params[:username]
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id 
+      redirect '/pools'
+    else 
+      redirect '/login' 
+    end 
+  end
   
-#   get '/users/:slug' do
-#     @user = User.find_by_slug(params[:slug])
-#     erb :'/users/show'
-#   end
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :'/users/show'
+  end
   
-#   get '/logout' do 
-#     if Helper.logged_in?(session)
-#       session.clear 
-#     else 
-#       redirect '/'
-#     end 
-#     redirect '/loggin'
-#   end 
+  get '/logout' do 
+    if Helper.logged_in?(session)
+      session.clear 
+    else 
+      redirect '/'
+    end 
+    redirect '/loggin'
+  end 
   
-# end 
+end 
 
 
 
--- db/migrate/20181115002428_create_users.rb
+++ db/migrate/20181115002428_create_users.rb
@@ -2,6 +2,8 @@ class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     creat_table :users do |t| 
       t.string :username 
+      t.string :email 
+      t.string :password_digest
     end
   end
 end
